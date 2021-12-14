cc2602c447e0bc8b83f5a0d7e17f93192a23083e
zbuck1 <95191417+zbuck1@users.noreply.github.com>
2021-12-14 09:10:49 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // your code here
+function shout(string) {
+    return string.toUpperCase();
+}
+function whisper(string) {
+    return string.toLowerCase();
+}
+function logShout(string) {
+    console.log("HELLO");
+    
+}
+function logWhisper(string) {
+    console.log("hello");
+}
+function sayHiToHeadphonedRoomate(string) {
+    if (string == 'hello')
+    return "I can't hear you!"
+    else if (string == 'HELLO')
+    return "YES INDEED!"
+    else (string == "I love you, Grandma.")
+    return "I would love to!"
+}
